a76d6fd90de9e9f7bd365a90fd9b17ded08f3d24
Agustín Barraque <agusbarraque@hotmail.com>
2022-12-16 15:05:45 -0300

    .

--- js/main.js
+++ js/main.js
@@ -4,15 +4,38 @@ $('.counter').counterUp({
     time: 1200
 })
 
-
+/* 
 const toggleButton = document.getElementsByClassName('NavToggle')[0]
 const navbarLinks = document.getElementsByClassName('Nav')[0]
 const boton = document.getElementsByClassName('open')[0]
 const botonA = document.getElementsByClassName('toggled')[0]
+*/
 
+/* 
+toggleButton.addEventListener('click', () => { 
+  navbarLinks.classList.toggle('active') 
+  boton.classList.toggle('hidden') 
+  botonA.classList.toggle('abierto') 
+})  
+ */
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-  boton.classList.toggle('hidden')
-  botonA.classList.toggle('abierto')
-})
+const bars = document.querySelector(".NavToggle");
+const navLinks = document.querySelector(".Nav");
+const links = document.querySelectorAll(".links");
+
+bars.addEventListener('click', () => {
+    navLinks.classList.toggle("active");
+    links.forEach(link => {
+        link.classList.toggle('active');
+    });
+    bars.classList.toggle("active");
+});
+
+navLinks.addEventListener('click', () => {
+    navLinks.classList.toggle("active");
+    links.forEach(link => {
+        link.classList.toggle('active');
+    });
+    bars.classList.toggle("active");
+});
+
