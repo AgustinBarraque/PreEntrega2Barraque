ddc3c8acdc0c13bb1339c7fa0ae5729a1946fb65
Agustín Barraque <agusbarraque@hotmail.com>
2022-12-16 05:24:04 -0300

    main

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ $('.counter').counterUp({
     delay: 10,
     time: 1200
 })
+
+
+const toggleButton = document.getElementsByClassName('NavToggle')[0]
+const navbarLinks = document.getElementsByClassName('Nav')[0]
+const boton = document.getElementsByClassName('open')[0]
+const botonA = document.getElementsByClassName('toggled')[0]
+
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+  boton.classList.toggle('hidden')
+  botonA.classList.toggle('abierto')
+})
